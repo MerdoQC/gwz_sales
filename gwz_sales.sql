1e24d18fbf8353921975f04154ab697f2afbe825
MerdoQC <mertarli35@gmail.com>
2025-12-04 23:17:56 +0300

    Update gwz_sales.sql
    tekrar 2 ondalık yuvarlamaya geri çevrildi.

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,6 @@
 SELECT
   date_date AS gun,
-  ROUND(SUM(turnover), 3) AS gunluk_ciro,purchase_cost
+  ROUND(SUM(turnover), 2) AS gunluk_ciro,purchase_cost
 FROM `platinum-loop-479214-e7.course14.gwz_sales`
 GROUP BY gun, purchase_cost
 ORDER BY gun;
